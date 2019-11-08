d677d01511bb8e55ee794b340d3d0a5b60523796
Zen Yip <zen.cfyip@gmail.com>
2019-11-08 19:08:21 +1100

    ready for heroku

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const config = require('./utils/config')
 
 const server = http.createServer(app)
 
-app.listen(config.PORT, () => {
-	console.log(`Server running on port ${config.PORT}`)
+const PORT = config.PORT || 3048
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
 })
