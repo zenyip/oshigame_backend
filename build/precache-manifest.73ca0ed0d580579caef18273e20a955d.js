49743f63bd9751c2cd33a19bdf8ecc5814d7b74d
Zen Yip <zen.cfyip@gmail.com>
2019-12-13 02:04:53 +1100

    frontend build updated

---
+++ build/precache-manifest.73ca0ed0d580579caef18273e20a955d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12db1e784c8c453832c6015472379b3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a4e095022a197adbb06",
+    "url": "/static/js/2.de0779ba.chunk.js"
+  },
+  {
+    "revision": "b6004fa876c016c42108",
+    "url": "/static/js/main.4b62dd1b.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a21572f55ce4982be25faa94aef1a8f.js"
+  "/precache-manifest.73ca0ed0d580579caef18273e20a955d.js"
 );
 
 self.addEventListener('message', (event) => {
