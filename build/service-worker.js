b30f86bf175a09f4be949d5beeab2e40d0d95e0b
Zen Yip <zen.cfyip@gmail.com>
2020-01-17 22:27:09 +1100

    frontend build updated

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1333d3a486ae61e5a130c48bf6a1cbef.js"
+  "/precache-manifest.77117b242d01992aeb761431994db6ea.js"
 );
 
 self.addEventListener('message', (event) => {
